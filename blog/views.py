83c85adfaf18fff0e9a944c96dfa1188f9bc7573
EngSuthail <e21395@eng.pdn.ac.lk>
2025-06-28 13:55:56 +0530

    Created django files to study

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world, You are at blog's index")
+
+def post(request):
+    return HttpResponse("You are on blog/post page")
+
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing post detail page And ID is {post_id}")
+
+def old_url_redirect(request):
+    return redirect("new_url")
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
+
+def redirect_me(request):
+    return redirect("index")
