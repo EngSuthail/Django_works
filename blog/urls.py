bbc80a4c1e3e56839c2a1e792b149be1d8ba30de
EngSuthail <e21395@eng.pdn.ac.lk>
2025-06-28 14:56:08 +0530

    check the html folder

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+
+app_name = 'blog'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("post", views.post, name="post"),
     path("post/<str:post_id>",views.detail, name="detail"),
-    path("new_url", views.new_url_view, name="new_url"),
-    path("old_url", views.old_url_redirect, name="old_url"),
-    path("redirect",  views.redirect_me, name="old_one")
+    path("new_something_url", views.new_url_view, name="new_page_url"),
+    path("old_url", views.old_url_redirect, name="old_url")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, You are at blog's index")
-
-def post(request):
-    return HttpResponse("You are on blog/post page")
+    return render(request, 'blog/index.html')
 
 def detail(request, post_id):
-    return HttpResponse(f"You are viewing post detail page And ID is {post_id}")
+    return render(request, 'blog/detail.html')
 
 def old_url_redirect(request):
-    return redirect("new_url")
+    return redirect(reverse('blog:new_page_url'))
 
 def new_url_view(request):
     return HttpResponse("This is the new URL")
-
-def redirect_me(request):
-    return redirect("index")
